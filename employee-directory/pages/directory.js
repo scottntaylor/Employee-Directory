e5e0e32e93281ba16aed52dc4a81f1f0ed689ef1
Scott Taylor <scottntaylor21@gmail.com>
2020-02-04 21:33:15 -0800

    Initial set up

---
+++ employee-directory/pages/directory.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Container from "../components/Container";
+import Col from "../components/Col";
+import Row from "../components/Row";
+
+const postEmployee = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+    };
+    return (
+        <div>
+            <div className="mt-4">
+                <h2>Add Employee</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <Container className="mt-3 px-5">
+                    <Row className="form-group">
+                        <Col size="12">
+                            <input className="form-control" type="text" placeholder="First Name" name="firstname" />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Col size="12">
+                            <input
+                                className="form-control"
+                                type="text"
+                                placeholder="Last Name"
+                                name="lastname"
+                            />
+                        </Col>
+                    </Row>
+                    <button className="btn btn-success" type="submit">
+                        Submit
+              </button>
+                </Container>
+                
+            </form>
+        </div>
+    );
+}
