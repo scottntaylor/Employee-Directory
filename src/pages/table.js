7db24c1ba3df752390077ccf5cd8edbee7ff1c92
Scott Taylor <scottntaylor21@gmail.com>
2020-02-06 18:12:48 -0800

    Ajax call working

--- src/pages/table.js
+++ src/pages/table.js
@@ -1,42 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import Container from "../components/Container";
+import api from "../Utils/api";
+
+class Table extends Component {
+
+  state = {
+    result: {}
+  }
 
 
-function Table (props){
-    return(
+  componentDidMount() {
+  this.gatherEmployees();
+  };
+
+  gatherEmployees() {
+  api.search()
+    .then(res => console.log(res.data))
+    // .then(res => this.setState({ result: res.data }))
+    .catch(err => console.log(err));
+    
+  // console.log(this.state.result);
+  };
+
+  render() {
+    return (
     <Container>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-</Container>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
     )
+  };
+
 };
 
 export default Table;
